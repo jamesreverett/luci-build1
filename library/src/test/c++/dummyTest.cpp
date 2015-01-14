e0ab2092ebd91dc03b23e8e784687afd95142ef4
Jamie Everett <jamesreverett@yahoo.com>
2015-01-14 16:18:27 -0700

    add nar + gtest example

---
+++ library/src/test/c++/dummyTest.cpp
@@ -0,0 +1,32 @@
+//-----------------------------------------------------------------------------
+//
+//      File:   dummyTest.cpp
+//
+// Copyright (C) Pixels Rising SRL November 10, 2013.
+//
+// This file was created as an unpublished copyright work. All rights reserved.
+//
+// This document and all the information it contains is confidential and
+// proprietary to Pixels Rising SRL. Hence, it might not be used, copied,
+// reproduced, transmitted or stored in any form or by any means, electronic,
+// recording, photocopying, mechanical or otherwise without prior written
+// permission of it's owner.
+//
+// THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
+// OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
+// MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
+// USE OF THIS SOFTWARE FOR COMMERCIAL DEVELOPMENT AND/OR EDUCATION IS SUBJECT
+// TO A CURRENT END USER LICENSE AGREEMENT WITH PIXELS RIDING SRL.
+//
+// This entire comment must accompany any portion of Pixels Rising source code
+// that is or moved from this file.
+//
+//      Created on: November 10, 2013, 8:41 PM
+//          Author: jfarkas
+//-----------------------------------------------------------------------------
+
+#include <gtest/gtest.h>
+#include <stdio.h>
+TEST(dummyTest, AllwaysTrueTest) {
+  EXPECT_EQ(1, 1);
+}
