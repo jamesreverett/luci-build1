c4d83741402e5f210fd6d6ffb6e1ea73a1d42478
Jamie Everett <jamesreverett@yahoo.com>
2015-01-16 15:08:12 -0700

    play with multiple tests

--- library/src/test/c++/input_readingTest.cpp
+++ library/src/test/c++/input_readingTest.cpp
@@ -2,9 +2,12 @@
 #include <stdio.h>
 #include "input_reading.h"
 
-TEST(InputReading, JamieWasHere) {
+TEST(InputReading, PrintExpected) {
   InputReading ir;
-
   EXPECT_EQ(0, ir.Print());
+}
+
+TEST(InputReading, PrintNotExpected) {
+  InputReading ir;
   EXPECT_NE(1, ir.Print());
 }
