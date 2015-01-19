2da7ef722c5b1c40f456532bf2dfc6be0b5e8d52
Jamie Everett <jamesreverett@yahoo.com>
2015-01-19 16:23:47 -0700

    expand the unit tests

--- library/src/test/c++/input_readingTest.cpp
+++ library/src/test/c++/input_readingTest.cpp
@@ -2,7 +2,27 @@
 #include <stdio.h>
 #include "input_reading.h"
 
-TEST(InputReading, UpdateAverageOpenness) {
+TEST(InputReading, DefaultAverageOpennessZero) {
     InputReading input;
     EXPECT_EQ(0.0, input.average_openness());
+}
+
+TEST(InputReading, AllNegativeAverageOpenness) {
+    InputReading input(-1.2, -1.3, 12345);
+    EXPECT_EQ(-1, input.average_openness());
+}
+
+TEST(InputReading, NegativeLeftAverageOpenness) {
+    InputReading input(-1.7, 2, 12345);
+    EXPECT_EQ(2, input.average_openness());
+}
+
+TEST(InputReading, NegativeRightAverageOpenness) {
+    InputReading input(3.4, -1.4, 12345);
+    EXPECT_EQ(3.4, input.average_openness());
+}
+
+TEST(InputReading, PositiveAverageOpenness) {
+    InputReading input(10, 20, 12345);
+    EXPECT_EQ(15, input.average_openness());
 }
