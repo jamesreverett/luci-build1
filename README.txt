92d3b505402b5d1fe0bf373a22a0fa4d2f27ccc3
Jamie Everett <jamesreverett@yahoo.com>
2015-01-27 14:02:34 -0700

    update example build/code

--- README.txt
+++ README.txt
@@ -1,52 +1,11 @@
 ==========
-GUIDELINES
-==========
-** Use maven nar philosophy for build, directory structure
-- http://maven-nar.github.io/index.html
-- http://maven-nar.github.io/philosophy.html
-Examples:
-- http://blog.bigpixel.ro/2012/07/building-cc-applications-with-maven/
-- https://accelconf.web.cern.ch/accelconf/icalepcs2011/papers/wepks026.pdf
-- https://www.google.ca/url?sa=t&rct=j&q=&esrc=s&source=web&cd=7&ved=0CEIQFjAG&url=https%3A%2F%2Fwww-acc.gsi.de%2Fwiki%2Fpub%2FFrontend%2FMeetingCMW130425%2FBuild_and_Continuous_Integration_for_Cpp.pptx&ei=8d62VPCCKcW_mwWxt4L4DQ&usg=AFQjCNHFtfBcOj1NtnfOuN16b0mle4arbg&sig2=TGVoFV0MLdeebdSmIytZSA&bvm=bv.83640239,d.dGY&cad=rjt
-
-
-** Use google code conventions
-- http://google-styleguide.googlecode.com/svn/trunk/cppguide.html
-Exceptions (!! NEED to discuss with Scott/Jay/?? !!):
-- 4 space indentation
-- .cpp file endings
-
-
-** Use googletest framework
-- https://code.google.com/p/googletest/
-- https://code.google.com/p/googletest/wiki/AdvancedGuide#Floating-Point_Comparison
-Sample:
-- http://blog.bigpixel.ro/2013/11/integrating-maven-nar-plugin-with-googles-googletest-unit-test-framework/
-
-
-** Use Travis CI
-- http://docs.travis-ci.com/user/languages/cpp/
-- http://docs.travis-ci.com/user/languages/java/
-Sample (using maven):
-- https://travis-ci.org/jamesreverett/luci-build1
-
-
-** Use Continuous Deployment (TODO - Research required)
-- foobar1
-- foobar2
-
-
-** IDE Recommendation (ie. project files checked-in)
-- TODO - test msvc
-- TODO - test netbeans 8.0 plugin
-
-
-** Relevant Trello Cards
-- https://trello.com/c/lYYw6lSZ - "Develop C/C++ build infrastructure on PAAS"
-
-==========
 NOTES
 ==========
+** Maven/nar for build, directory structure
+** Google Test framework
+** Travis CI
+** Git Releases
+
 - http://www.cplusplus.com/
 - http://www.boost.org/
 - http://geosoft.no/development/cpppractice.html
--- library/src/main/c++/input_reading.cpp
+++ library/src/main/c++/input_reading.cpp
@@ -1,79 +1,23 @@
 #include <stdio.h>
-
 #include "input_reading.h"
 
 InputReading::InputReading() {
-    left_openness_      = 0.0;
-    right_openness_     = 0.0;
-    average_openness_   = 0.0;
-    perclos_            = 0;
-    device_timestamp_   = 0;
-    epoch_time_         = 0;
-    EvaluateOpenness();
+    left_      = 0.0;
+    right_     = 0.0;
+    average_   = 0.0;
+    evaluate();
 }
 
-InputReading::InputReading(float left, float right, long timestamp) {
-    left_openness_      = left;
-    right_openness_     = right;
-    average_openness_   = 0.0;
-    perclos_            = 0;
-    device_timestamp_   = timestamp;
-    epoch_time_         = 0;
-    EvaluateOpenness();
+float InputReading::average() {
+    return average_;
 }
 
-float InputReading::average_openness() {
-    return average_openness_;
-}
-
-float InputReading::left_openness() {
-    return left_openness_;
-}
-void InputReading::set_left_openness(float val) {
-    left_openness_= val;
-    EvaluateOpenness();
-}
-
-float InputReading::right_openness() {
-    return right_openness_;
-}
-void InputReading::set_right_openness(float val) {
-    right_openness_= val;
-    EvaluateOpenness();
-}
-
-long InputReading::device_timestamp() {
-    return device_timestamp_;
-}
-void InputReading::set_device_timestamp(long val) {
-    device_timestamp_ = val;
-}
-
-long InputReading::epoch_time() {
-    return epoch_time_;
-}
-void InputReading::set_epoch_time(long val) {
-    epoch_time_ = val;
-}
-
-
-
-void InputReading::EvaluateOpenness() {
-    if (left_openness_ < 0.0 && right_openness_ < 0.0) {
-        average_openness_ = -1;
-    } else if (left_openness_ < 0.0 || right_openness_ < 0.0) {
-        average_openness_ = (right_openness_ < 0.0) ? left_openness_ : right_openness_;
+void InputReading::evaluate() {
+    if (left_ < 0.0 && right_ < 0.0) {
+        average_ = -1;
+    } else if (left_ < 0.0 || right_ < 0.0) {
+        average_ = (right_ < 0.0) ? left_ : right_;
     } else {
-        average_openness_ = (right_openness_ + left_openness_) / 2;
+        average_ = (right_ + left_) / 2;
     }
-
-    if (average_openness_ < kEyeRatioBarrier) {
-        // Openness is less a positive barrier - ie can be zero or negative
-        perclos_ = 1;
-    }
-}
-
-void InputReading::PrintJSON() const {
-  printf("{left_ratio: %f, right_ratio: %f, used_ratio: %f, perclos_reading: %d, timestamp: %d, system_time: %d",
-         left_openness_, right_openness_, average_openness_, perclos_, epoch_time_, device_timestamp_);
 }
